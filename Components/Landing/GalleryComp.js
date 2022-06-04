b6dfde85faa6a7a2468e58c25c92c4a40e7aaa4a
lutfor779 <mdlrrahman779@gmail.com>
2022-06-05 00:44:12 +0600

    modal added

--- Components/Landing/GalleryComp.js
+++ Components/Landing/GalleryComp.js
@@ -1,5 +1,8 @@
+import Image from "next/image";
 import React from "react";
-import styled from "styled-components";
+import { Col, Row } from "react-bootstrap";
+import gallerySlider1 from "../../public/assets/images/gallery/gallery-slider-1.svg";
+import gallerySlider2 from "../../public/assets/images/gallery/gallery-slider-2.svg";
 import img1 from "../../public/assets/images/gallery/gallery1.svg";
 import img2 from "../../public/assets/images/gallery/gallery2.svg";
 import img3 from "../../public/assets/images/gallery/gallery3.svg";
@@ -8,331 +11,154 @@ import img5 from "../../public/assets/images/gallery/gallery5.svg";
 import img6 from "../../public/assets/images/gallery/gallery6.svg";
 import img7 from "../../public/assets/images/gallery/gallery7.svg";
 import img8 from "../../public/assets/images/gallery/gallery8.svg";
-import gallerySlider1 from "../../public/assets/images/gallery/gallery-slider-1.svg";
-import gallerySlider2 from "../../public/assets/images/gallery/gallery-slider-2.svg";
-import Image from "next/image";
-import { Row, Col } from "react-bootstrap";
+import Modal from "../Modal";
 
 function GalleryComp() {
-  return (
-    <>
-      <Row className="g-3 mt-1">
-        <Col xs={12} md={6}>
-          <div className="gallery__slider-img">
-            <Image src={gallerySlider1} layout="responsive" alt="" />
-          </div>
-        </Col>
-        <Col xs={12} md={6}>
-          <Row className="g-3">
-            <Col xs={6}>
-              <Image src={img1} layout="responsive" alt="" />
-            </Col>
-            <Col xs={6}>
-              <Image src={img2} layout="responsive" alt="" />
-            </Col>
-            <Col xs={6}>
-              <Image src={img3} layout="responsive" alt="" />
-            </Col>
-            <Col xs={6}>
-              <Image src={img4} layout="responsive" alt="" />
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-      <Row className="g-3 mt-1">
-        <Col xs={12} md={6}>
-          <Row className="g-3">
-            <Col xs={6}>
-              <Image src={img5} layout="responsive" alt="" />
-            </Col>
-            <Col xs={6}>
-              <Image src={img6} layout="responsive" alt="" />
-            </Col>
-            <Col xs={6}>
-              <Image src={img7} layout="responsive" alt="" />
-            </Col>
-            <Col xs={6}>
-              <Image src={img8} layout="responsive" alt="" />
-            </Col>
-          </Row>
-        </Col>
-        <Col xs={12} md={6}>
-          <div className="gallery__slider-img">
-            <Image src={gallerySlider2} layout="responsive" />
-          </div>
-        </Col>
-      </Row>
-    </>
-  );
+	const [img, setImage] = React.useState(null);
+	const [open, setOpen] = React.useState(false);
+
+	return (
+		<>
+			<Row className="g-3 mt-1">
+				<Col xs={12} md={6}>
+					<div className="gallery__slider-img">
+						<Image
+							src={gallerySlider1}
+							layout="responsive"
+							alt=""
+							style={{ cursor: "pointer" }}
+							onClick={() => {
+								setImage(gallerySlider1);
+								setOpen(!open);
+							}}
+						/>
+					</div>
+				</Col>
+				<Col xs={12} md={6}>
+					<Row className="g-3">
+						<Col xs={6}>
+							<Image
+								src={img1}
+								layout="responsive"
+								alt=""
+								style={{ cursor: "pointer" }}
+								onClick={() => {
+									setImage(img1);
+									setOpen(!open);
+								}}
+							/>
+						</Col>
+						<Col xs={6}>
+							<Image
+								src={img2}
+								layout="responsive"
+								alt=""
+								style={{ cursor: "pointer" }}
+								onClick={() => {
+									setImage(img2);
+									setOpen(!open);
+								}}
+							/>
+						</Col>
+						<Col xs={6}>
+							<Image
+								src={img3}
+								layout="responsive"
+								alt=""
+								style={{ cursor: "pointer" }}
+								onClick={() => {
+									setImage(img3);
+									setOpen(!open);
+								}}
+							/>
+						</Col>
+						<Col xs={6}>
+							<Image
+								src={img4}
+								layout="responsive"
+								alt=""
+								style={{ cursor: "pointer" }}
+								onClick={() => {
+									setImage(img4);
+									setOpen(!open);
+								}}
+							/>
+						</Col>
+					</Row>
+				</Col>
+			</Row>
+
+			<Row className="g-3 mt-1">
+				<Col xs={12} md={6}>
+					<Row className="g-3">
+						<Col xs={6}>
+							<Image
+								src={img5}
+								layout="responsive"
+								alt=""
+								style={{ cursor: "pointer" }}
+								onClick={() => {
+									setImage(img5);
+									setOpen(!open);
+								}}
+							/>
+						</Col>
+						<Col xs={6}>
+							<Image
+								src={img6}
+								layout="responsive"
+								alt=""
+								style={{ cursor: "pointer" }}
+								onClick={() => {
+									setImage(img6);
+									setOpen(!open);
+								}}
+							/>
+						</Col>
+						<Col xs={6}>
+							<Image
+								src={img7}
+								layout="responsive"
+								alt=""
+								style={{ cursor: "pointer" }}
+								onClick={() => {
+									setImage(img7);
+									setOpen(!open);
+								}}
+							/>
+						</Col>
+						<Col xs={6}>
+							<Image
+								src={img8}
+								layout="responsive"
+								alt=""
+								style={{ cursor: "pointer" }}
+								onClick={() => {
+									setImage(img8);
+									setOpen(!open);
+								}}
+							/>
+						</Col>
+					</Row>
+				</Col>
+				<Col xs={12} md={6}>
+					<div className="gallery__slider-img">
+						<Image
+							src={gallerySlider2}
+							alt=""
+							layout="responsive"
+							style={{ cursor: "pointer" }}
+							onClick={() => {
+								setImage(gallerySlider2);
+								setOpen(!open);
+							}}
+						/>
+					</div>
+				</Col>
+			</Row>
+
+			{open && <Modal src={img} setOpen={setOpen} />}
+		</>
+	);
 }
 export default GalleryComp;
-{
-  // <GalleryContainer>
-  //   <div className="inner-container">
-  //     <p className="title">Gallery</p>
-  //     <ImageContainer>
-  //       {/* <Upper> */}
-  //       <div className="row">
-  //         <div className="col-12 col-md-6 p-0">
-  //           <div className="m-2 d-flex justify-content-center">
-  //             <Image src={gallerySlider1} alt="" />
-  //           </div>
-  //         </div>
-  //         <div className="col-12 col-md-6 p-0">
-  //           <div className="">
-  //             <div className=" row">
-  //               <div className="col-6 d-flex justify-content-end p-1">
-  //                 <Image src={img1} alt="" />
-  //               </div>
-  //               <div className="col-6 d-flex justify-content-start p-1">
-  //                 <Image src={img2} alt="" />
-  //               </div>
-  //               <div className="col-6 d-flex justify-content-end p-1">
-  //                 <Image src={img3} alt="" />
-  //               </div>
-  //               <div className="col-6 d-flex justify-content-start p-1">
-  //                 <Image src={img4} alt="" />
-  //               </div>
-  //             </div>
-  //           </div>
-  //           {/* <div className="grid-upper m-1">
-  //             <Image src={img2} alt="" />
-  //           </div> */}
-  //           {/* <div className="grid-lower m-1">
-  //             <Image src={img3} alt="" />
-  //             <Image src={img4} alt="" />
-  //           </div> */}
-  //         </div>
-  //       </div>
-  //       {/* </Upper> */}
-  //       {/* <Lower> */}
-  //       <div className="row">
-  //         <div className="col-12 col-md-6 p-0 order-2 order-md-1">
-  //           <div className="">
-  //             <div className="row">
-  //               <div className="col-6 d-flex justify-content-end p-1">
-  //                 <Image src={img5} alt="" />
-  //               </div>
-  //               <div className="col-6 d-flex justify-content-start p-1">
-  //                 <Image src={img6} alt="" />
-  //               </div>
-  //               <div className="col-6 d-flex justify-content-end p-1">
-  //                 <Image src={img7} alt="" />
-  //               </div>
-  //               <div className="col-6  d-flex justify-content-start p-1">
-  //                 <Image src={img8} alt="" />
-  //               </div>
-  //             </div>
-  //           </div>
-  //         </div>
-  //         <div className="col-12 col-md-6 p-0 order-1 order-md-2">
-  //           <div className="d-flex justify-content-center">
-  //             <Image src={gallerySlider2} alt="" />
-  //           </div>
-  //         </div>
-  //       </div>
-  //       {/* </Lower> */}
-  //     </ImageContainer>
-  //   </div>
-  // </GalleryContainer>
-  // const GalleryContainer = styled.div`
-  //   background-color: var(--secondary);
-  //   .title {
-  //     font-size: 50px;
-  //     font-weight: bold;
-  //     opacity: 0.9;
-  //     color: #Fff8f0;
-  //     font-family: poppins;
-  //     letter-spacing: 5px;
-  //     margin-bottom: 20px;
-  //     font-family: "IM Fell Double Pica", serif;
-  //   }
-  //   .inner-container {
-  //     width: 80%;
-  //     margin: 0 auto;
-  //   }
-  //   padding: 50px 0;
-  // `;
-  // const ImageContainer = styled.div`
-  //   img {
-  //     max-width: 100%;
-  //     border-radius: 10px;
-  //   }
-  //   .slider {
-  //     width: 45%;
-  //     img {
-  //       width: 98%;
-  //     }
-  //   }
-  //   .grid {
-  //     display: flex;
-  //     width: 45%;
-  //     img {
-  //       width: 95%;
-  //       margin: 4px;
-  //     }
-  //   }
-  // `;
-  // const Upper = styled.div`
-  //   display: flex;
-  //   width: 100%;
-  //   margin-bottom: 15px;
-  // `;
-  // const Lower = styled.div`
-  //   display: flex;
-  //   width: 100%;
-  // `;
-}
-
-{
-  /* <div className="row">
-  <div className="col-6 p-0 bg-warning">
-	<Image src={gallerySlider1} alt="" />
-  </div>
-  <div className="col-6 p-0 bg-danger">
-	<div className="d-flex">
-	  <div className="w-50 d-flex justify-content-center">
-		<Image
-		  src={img1}
-		  alt=""
-		  className=""
-		  style={{ width: "100%" }}
-		/>
-	  </div>
-	  <div className="w-50 d-flex justify-content-center">
-		<Image
-		  src={img2}
-		  alt=""
-		  className=""
-		  style={{ width: "100%" }}
-		/>
-	  </div>
-	</div>
-	<div className="d-flex">
-	  <div className="w-50 d-flex justify-content-center">
-		<Image
-		  src={img1}
-		  alt=""
-		  className=""
-		  style={{ width: "100%" }}
-		/>
-	  </div>
-	  <div className="w-50 d-flex justify-content-center">
-		<Image
-		  src={img2}
-		  alt=""
-		  className=""
-		  style={{ width: "100%" }}
-		/>
-	  </div>
-	</div>
-	<div className="col-6 p-0">
-		<Image
-		  src={img1}
-		  alt=""
-		  className=""
-		  style={{ width: "100%" }}
-		/>
-	  </div>
-	  <div className="col-6 p-0">
-		<Image
-		  src={img2}
-		  alt=""
-		  className=""
-		  style={{ width: "100%" }}
-		/>
-	  </div> 
-	 <div className="col-6 p-0">
-		<Image
-		  src={img3}
-		  alt=""
-		  className=""
-		  style={{ width: "100%" }}
-		/>
-	  </div>
-	  <div className="col-6 p-0">
-		<Image
-		  src={img4}
-		  alt=""
-		  className=""
-		  style={{ width: "100%" }}
-		/>
-	  </div>
-  </div>
-  </div>
-  <div className="row">
-  <div className="col-6 p-0 bg-warning">
-	<Image src={gallerySlider1} alt="" />
-  </div>
-  <div className="col-6 p-0 bg-danger">
-	<div className="d-flex">
-	  <div className="w-50 d-flex justify-content-center">
-		<Image
-		  src={img1}
-		  alt=""
-		  className=""
-		  style={{ width: "100%" }}
-		/>
-	  </div>
-	  <div className="w-50 d-flex justify-content-center">
-		<Image
-		  src={img2}
-		  alt=""
-		  className=""
-		  style={{ width: "100%" }}
-		/>
-	  </div>
-	</div>
-	<div className="d-flex">
-	  <div className="w-50 d-flex justify-content-center">
-		<Image
-		  src={img1}
-		  alt=""
-		  className=""
-		  style={{ width: "100%" }}
-		/>
-	  </div>
-	  <div className="w-50 d-flex justify-content-center">
-		<Image
-		  src={img2}
-		  alt=""
-		  className=""
-		  style={{ width: "100%" }}
-		/>
-	  </div>
-	</div>
-	<div className="col-6 p-0">
-		<Image
-		  src={img1}
-		  alt=""
-		  className=""
-		  style={{ width: "100%" }}
-		/>
-	  </div>
-	  <div className="col-6 p-0">
-		<Image
-		  src={img2}
-		  alt=""
-		  className=""
-		  style={{ width: "100%" }}
-		/>
-	  </div> */
-}
-//  <div className="col-6 p-0">
-//       <Image
-//         src={img3}
-//         alt=""
-//         className=""
-//         style={{ width: "100%" }}
-//       />
-//     </div>
-//     <div className="col-6 p-0">
-//       <Image
-//         src={img4}
-//         alt=""
-//         className=""
-//         style={{ width: "100%" }}
-//       />
-//     </div>
-// </div>
-// </div>
---
+++ Components/Modal.js
@@ -0,0 +1,71 @@
+import Image from "next/image";
+import React, { useEffect } from "react";
+import styled from "styled-components";
+
+const Modal = ({ src, setOpen }) => {
+	useEffect(() => {
+		const modal = document.getElementById("modal");
+		window.onclick = function (event) {
+			if (event.target == modal) {
+				setOpen(false);
+			}
+		};
+	});
+
+	return (
+		<ModalContainer id="modal">
+			<Content>
+				<span className="close" onClick={() => setOpen(false)}>
+					&times;
+				</span>
+				<Image src={src} alt="Details Image" />
+			</Content>
+		</ModalContainer>
+	);
+};
+
+export default Modal;
+
+const ModalContainer = styled.div`
+	display: flex;
+	position: fixed;
+	z-index: 9999;
+	left: 0;
+	top: 0;
+	width: 100%;
+	height: 100%;
+	overflow: hidden;
+	background-color: rgb(0, 0, 0);
+	background-color: rgba(0, 0, 0, 0.6);
+	padding: 15px;
+`;
+const Content = styled.div`
+	background-color: #fefefe;
+	margin: auto;
+	padding: 15px;
+	border: 1px solid #888;
+	width: max-content;
+	position: relative;
+	padding-bottom: 8px;
+	border-radius: 15px;
+
+	.close {
+		position: absolute;
+		top: -7px;
+		right: 7px;
+		color: #aaa;
+		font-size: 28px;
+		font-weight: bold;
+		z-index: 99999;
+	}
+	.close:hover,
+	.close:focus {
+		color: black;
+		text-decoration: none;
+		cursor: pointer;
+	}
+
+	img {
+		min-height: 50vh !important;
+	}
+`;
