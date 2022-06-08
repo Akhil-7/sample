847706c32cef78903ea662a9546d349bf6fb510d
lutfor779 <mdlrrahman779@gmail.com>
2022-06-09 01:03:50 +0600

    course details page details added

--- pages/course_details.js
+++ pages/course_details.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import { BsFillStarFill } from "react-icons/bs";
+import { BsCheck, BsFillStarFill } from "react-icons/bs";
 // import img8 from "../public/assets/images/Vector (8).png";
 import styled from "styled-components";
 import img7 from "../public/assets/images/course-detials-bg-full.svg";
@@ -30,6 +30,55 @@ const CourseDetail = () => {
 					</div>
 				</div>
 			</Banner>
+			<DetailsContainer>
+				<Details>
+					<p className="tag">Details</p>
+					<h1>Description</h1>
+					<p>
+						{
+							"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit, nulla quidem voluptates quia molestias beatae consequuntur iste veritatis temporibus eligendi, cumque aliquid alias nemo facere consectetur magni aspernatur maiores laudantium!"
+						}
+					</p>
+
+					<h3>What is Included</h3>
+					<Item>
+						<p>
+							<BsCheck className="icon" />
+						</p>
+						<p>
+							Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+							Inventore, nam?
+						</p>
+					</Item>
+					<Item>
+						<p>
+							<BsCheck className="icon" />
+						</p>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque,
+							suscipit.
+						</p>
+					</Item>
+					<Item>
+						<p>
+							<BsCheck className="icon" />
+						</p>
+						<p>
+							Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+							Repellat, aperiam!
+						</p>
+					</Item>
+					<Item>
+						<p>
+							<BsCheck className="icon" />
+						</p>
+						<p>
+							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa,
+							quas.
+						</p>
+					</Item>
+				</Details>
+			</DetailsContainer>
 		</CourseDetails>
 	);
 };
@@ -37,8 +86,43 @@ const CourseDetail = () => {
 export default CourseDetail;
 const CourseDetails = styled.div`
 	margin-top: 77px;
-`;
+	h1 {
+		font-family: "IM Fell Double Pica", serif;
+		font-size: 40px;
+		font-weight: 500;
+		@media (max-width: 700px) {
+			font-size: 27px;
+		}
+	}
 
+	h3 {
+		font-family: "IM Fell Double Pica", serif;
+		font-size: 27px;
+		font-weight: 500;
+		margin-top: 15px;
+		@media (max-width: 700px) {
+			font-size: 1rem;
+		}
+	}
+
+	p {
+		font-family: "IM Fell Double Pica", serif;
+		font-size: 1rem;
+		@media (max-width: 700px) {
+			font-size: 0.8rem;
+		}
+	}
+	span {
+		font-family: "IM Fell Double Pica", serif;
+		font-size: 25px;
+		font-weight: 500;
+		color: var(--primary);
+		@media (max-width: 700px) {
+			font-size: 17px;
+		}
+	}
+`;
+// font-family: "IM Fell Double Pica", serif;
 const Banner = styled.div`
 	position: relative;
 	min-height: 100%;
@@ -66,29 +150,7 @@ const Banner = styled.div`
 		display: flex;
 		flex-direction: column;
 		justify-content: space-evenly;
-		h1 {
-			font-family: "IM Fell Double Pica", serif;
-			font-size: 40px;
-			font-weight: 500;
-			@media (max-width: 700px) {
-				font-size: 27px;
-			}
-		}
-		p {
-			font-size: 1rem;
-			@media (max-width: 700px) {
-				font-size: 0.8rem;
-			}
-		}
-		span {
-			font-family: "IM Fell Double Pica", serif;
-			font-size: 25px;
-			font-weight: 500;
-			color: var(--primary);
-			@media (max-width: 700px) {
-				font-size: 17px;
-			}
-		}
+
 		.rating {
 			display: flex;
 			align-items: center;
@@ -105,5 +167,26 @@ const Banner = styled.div`
 				}
 			}
 		}
+	}
+`;
+const DetailsContainer = styled.div`
+	background: var(--secondary);
+	margin-top: -7px;
+	color: #fff;
+	padding: 25px 0;
+`;
+const Details = styled.div`
+	width: 80%;
+	margin: auto;
+	.tag {
+		letter-spacing: 5px;
+		text-transform: uppercase;
+		font-size: 10px;
+	}
+`;
+const Item = styled.div`
+	display: flex;
+	.icon {
+		font-size: 1.2rem;
 	}
 `;
