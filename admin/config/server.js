0d4a9c9cb0b0e5868371816d7890e45d24934eb9
Manas Mahanand <robot14.mahanand@gmail.com>
2022-06-08 20:04:28 +0530

    changed public url

--- admin/config/server.js
+++ admin/config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env("PUBLIC_URL", "http://65.20.75.248"),
+  url: env("PUBLIC_URL", "http://65.20.75.248:1337"),
   app: {
     keys: env.array('APP_KEYS'),
   },
